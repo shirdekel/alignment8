136a2ea3572d71af4d41e7c2697cdaa725ea4e1f
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-17 19:33:13 +1000

    Add data

---
+++ R/alignment8.R
@@ -0,0 +1,29 @@
+#' alignment8data
+#' A data package for alignment experiment 8.
+#' @docType package
+#' @aliases alignment8data-package
+#' @title Package Title
+#' @name alignment8data
+#' @description A description of the data package
+#' @details Use \code{data(package='alignment8data')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{data}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name data
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{rowwise_df} containing the following fields:
+#' \describe{
+#' \item{time_elapsed}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment8data}
+NULL
