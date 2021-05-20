6c966793375f342e093daf3acf056d6fa7c2c874
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-20 10:35:06 +1000

    Export simulation and power analysis

--- data-raw/documentation.R
+++ data-raw/documentation.R
@@ -14,6 +14,7 @@ NULL
 
 
 
+
 #' Detailed description of the data
 #' @name data
 #' @docType data
@@ -26,6 +27,61 @@ NULL
 #' @seealso
 #' \link{alignment8data}
 NULL
+#' alignment8
+#' A data package for alignment8.
+#' @docType package
+#' @aliases alignment8-package
+#' @title Package Title
+#' @name alignment8
+#' @description A description of the data package
+#' @details Use \code{data(package='alignment8')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{plot_simulation}
+#' \link{power}
+NULL
 
 
 
+
+#' Detailed description of the data
+#' @name plot_simulation
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{gg} containing the following fields:
+#' \describe{
+#' \item{data}{}
+#' \item{layers}{}
+#' \item{scales}{}
+#' \item{mapping}{}
+#' \item{theme}{}
+#' \item{coordinates}{}
+#' \item{facet}{}
+#' \item{plot_env}{}
+#' \item{labels}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment8}
+#' \link{power}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name power
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' \describe{
+#' \item{power_curve}{}
+#' \item{n}{}
+#' \item{n_total}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment8}
+#' \link{plot_simulation}
+NULL
